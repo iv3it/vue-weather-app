19343e7747dfde875cfdba6f05f6aa8e6b6c5092
Kinga <kinga.sabat@gmail.com>
2022-02-04 17:18:43 +0100

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  },
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../variables';
+
+</style>
